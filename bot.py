69b5d036a42797a3f35591707623841b8c62010e
WieeRd_laptop <wowluke@naver.com>
2021-08-07 17:28:21 +0900

    modified debug print()s

--- bot.py
+++ bot.py
@@ -20,6 +20,7 @@ class IroBot(commands.Bot):
         self.newbie_role: discord.Role
 
     async def on_ready(self):
+        print(f"{self.user} is now online")
         guild = self.get_guild(config.GUILD_ID)
         assert guild # because return value can be None
         role = guild.get_role(config.NEWBIE_ROLE_ID)
@@ -27,15 +28,16 @@ class IroBot(commands.Bot):
 
         self.working_guild = guild
         self.newbie_role = role
-        print(guild)
-        print(role)
+        print(f"Working guild: {guild} ({guild.member_count} users)")
+        print(f"Newbie role: {role}")
 
     async def close(self):
+        print("Bye")
         await self.pool.close()
 
     # 1. 서버에 들어오면 미인증 역할을 준다
     async def on_member_join(self, member: discord.Member):
-        print(f"Joined {member}")
+        print(f"New member: {member} ({member.id})")
         await member.add_roles(self.newbie_role)
 
     # 2. 미인증 유저의 챗은 모두 삭제된다
@@ -46,6 +48,8 @@ class IroBot(commands.Bot):
         if self.newbie_role in msg.author.roles:
             await msg.delete()
 
+        # without this, bot commands won't work
+        # but this is not a bot for commands anyway
         # await self.process_commands(msg)
 
     # 3. 서버 닉네임 유지
@@ -60,18 +64,16 @@ class IroBot(commands.Bot):
 
     # 4. 나간 유저 데이터 삭제
     async def on_member_remove(self, member: discord.Member):
-        print(f"Leaved {member}")
+        print(f"Member left: {member} ({member.id})")
         async with await self.pool.Connection() as conn:
             async with conn.cursor() as cur:
                 await cur.execute("DELETE FROM linked_account WHERE discord=%s", member.id)
             await conn.commit()
 
-intents=discord.Intents.default()
-intents.members=True
 bot = IroBot(
     pool = tormysql.ConnectionPool(**config.SQL),
     command_prefix=config.COMMAND_PREFIX,
-    intents=intents,
+    intents=discord.Intents.all(),
     help_command=None
 )
 
