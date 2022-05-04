dc50ad466b6ec84f71f9820bf565b404a52e6740
peunsu <peunsu55@gmail.com>
2022-05-05 01:38:35 +0900

    Added /verify and /unverify commands.

--- app_v2.py
+++ app_v2.py
@@ -2,39 +2,143 @@ import interactions
 
 import tormysql
 import re
+import redis
 import time, datetime
-
-from os import environ
-from dotenv import load_dotenv
 
 from mojang import MojangAPI
 from mcstatus import JavaServer
 
+import config
 
-load_dotenv()
+TOKEN = config.TOKEN
+GUILD_ID = config.GUILD_ID
+NEWBIE_ROLE_ID = config.NEWBIE_ROLE_ID
+EMBED_COLOR = config.EMBED_COLOR
+SQL = config.SQL
+REDIS = config.REDIS
 
-GUILD_ID = 330997213255827457
-EMBED_COLOR = 15844367
+MSG_MATCH = "마인크래프트 계정 `{mcnick}` 이/가 성공적으로 인증되었습니다."
+MSG_DISMATCH = "인증번호가 일치하지 않습니다."
 
-SQL = {
-    'host': "localhost",
-    'port': 3306,
-    'user': "root",
-    'passwd': "",
-    'db': "mcauth",
-}
+MSG_UNVERIFY_FAIL = "입력한 문구가 올바르지 않습니다. 계정 인증 해제를 취소합니다."
+MSG_UNVERIFY_SUCCESS = "계정 인증이 성공적으로 해제되었습니다."
 
 MSG_INVAILD_UUID = "유효하지 않은 uuid입니다. 32자리의 uuid를 대시(-)를 포함하여 정확히 입력해주세요."
 MSG_INVALID_NAME = "유효하지 않은 닉네임입니다. 마인크래프트 닉네임을 정확히 입력해주세요."
+MSG_INVAILD_CODE = "유효하지 않은 인증코드입니다. 인증코드는 띄어쓰기 없이 6자리 숫자로 입력해주세요."
 
+MSG_DUPLICATE = "마인크래프트 계정 `{mcnick}` 은 이미 인증된 계정입니다. 본인이 인증한 것이 아니라면 고객센터에 문의해주세요."
+MSG_BANNED = "마인크래프트 계정 `{mcnick}` 은/는 차단된 계정입니다. 차단된 계정으로는 인증하실 수 없습니다."
+
+MSG_SERVER_DOWN = "서버 정보를 불러올 수 없습니다."
+
+SQL_CHECK_DUPLICATE = "SELECT * FROM linked_account WHERE mcuuid=%s"
+SQL_CHECK_BLACK = "SELECT * FROM blacklist WHERE mcuuid=%s"
+SQL_INSERT = "INSERT INTO linked_account(discord,mcuuid) values (%s, %s)"
+SQL_DELETE = "DELETE FROM linked_account WHERE discord=%s"
+
+REGEX_CODE = re.compile(r'\d{6}')
 UUID_REGEX_CODE = re.compile(r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}')
 
 start_time = time.time()
 pool = tormysql.ConnectionPool(**SQL)
-bot = interactions.Client(token=environ["TOKEN"], intents=interactions.Intents.ALL)
+rd = redis.StrictRedis(**REDIS)
+bot = interactions.Client(token=TOKEN, intents=interactions.Intents.ALL)
 
 def get_footer():
     return time.strftime(f"%Y.%m.%d. %H:%M:%S", time.localtime()) + " [개발 버전]"
+
+@bot.command(
+    name="verify",
+    description="마인크래프트 계정을 인증합니다.",
+    scope=GUILD_ID
+)
+async def verify(ctx: interactions.CommandContext):
+    modal = interactions.Modal(
+        title="MRS 마인크래프트 계정 인증",
+        custom_id="modal_verify",
+        components=[
+            interactions.TextInput(
+                style=interactions.TextStyleType.SHORT,
+                label="마인크래프트 닉네임",
+                custom_id="mcnick",
+                required=True,
+                min_length=3,
+                max_length=16
+            ),
+            interactions.TextInput(
+                style=interactions.TextStyleType.SHORT,
+                label="인증코드 (띄어쓰기 없이 입력)",
+                custom_id="code",
+                required=True,
+                placeholder=123456,
+                min_length=6,
+                max_length=6
+            )
+        ]
+    )
+    
+    await ctx.popup(modal)
+
+@bot.modal("modal_verify")
+async def verify_response(ctx: interactions.CommandContext, mcnick: str, code: str):
+    if not REGEX_CODE.match(code):
+        return await ctx.send(MSG_INVAILD_CODE, ephemeral=True)
+    
+    if rd.exists(mcnick):
+        realcode = rd.hget(mcnick, "code").decode("UTF-8")
+        uuid = rd.hget(mcnick, "UUID").decode("UTF-8")
+        async with await pool.Connection() as conn:
+            async with conn.cursor() as cur:
+                if await cur.execute(SQL_CHECK_DUPLICATE, (uuid, )):
+                    return await ctx.send(MSG_DUPLICATE.format(mcnick=mcnick), ephemeral=True)
+                if await cur.execute(SQL_CHECK_BLACK, (uuid, )):
+                    return await ctx.send(MSG_BANNED.format(mcnick=mcnick), ephemeral=True)
+        if realcode == code:
+            await ctx.author.modify(nick=mcnick)
+            async with await pool.Connection() as conn:
+                async with conn.cursor() as cur:
+                    await cur.execute(SQL_INSERT, (int(ctx.author.id), uuid))
+                await conn.commit()
+            await ctx.send(MSG_MATCH.format(mcnick=mcnick), ephemeral=True)
+        else:
+            await ctx.send(MSG_DISMATCH, ephemeral=True)
+    else:
+        await ctx.send(MSG_INVALID_NAME, ephemeral=True)
+
+@bot.command(
+    name="unverify",
+    description="마인크래프트 계정 인증을 해제합니다.",
+    scope=GUILD_ID
+)
+async def unverify(ctx: interactions.CommandContext):
+    modal = interactions.Modal(
+        title="MRS 계정 인증 해제",
+        custom_id="modal_unverify",
+        components=[
+            interactions.TextInput(
+                style=interactions.TextStyleType.SHORT,
+                label="인증을 해제하시려면 본인의 닉네임을 정확히 입력해주세요.",
+                custom_id="check_msg",
+                required=True,
+                placeholder=ctx.author.nick
+            )
+        ]
+    )
+    
+    await ctx.popup(modal)
+
+@bot.modal("modal_unverify")
+async def unverify_response(ctx: interactions.CommandContext, check_msg: str):
+    if not check_msg == ctx.author.nick:
+        return await ctx.send(MSG_UNVERIFY_FAIL, ephemeral=True)
+    
+    await ctx.author.remove_role(role=NEWBIE_ROLE_ID)
+    async with await pool.Connection() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute(SQL_DELETE, (int(ctx.author.id), ))
+        await conn.commit()
+    await ctx.send(MSG_UNVERIFY_SUCCESS, ephemeral=True)
 
 @bot.command(
     name="status",
@@ -75,7 +179,7 @@ async def query(ctx: interactions.CommandContext, ip: str = None):
     try:
         server = JavaServer.lookup(ip).status()
     except:
-        return await ctx.send("서버 정보를 불러올 수 없습니다.", ephemeral=True)
+        return await ctx.send(MSG_SERVER_DOWN, ephemeral=True)
     
     
     await ctx.send(embeds=interactions.Embed(
